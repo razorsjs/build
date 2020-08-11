5dd4d71d0393ab171a6ae0ca3dbcd1f360e83c0b
liujia02 <bit_liujia@163.com>
2020-08-11 15:32:02 +0800

    build(ship): add monorepo

--- ship.config.js
+++ ship.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   publishCommand: ({ defaultCommand }) => `${defaultCommand} --access public`,
+  monorepo: {
+    mainVersionFile: 'lerna.json', // or `lerna.json`, or whatever a json file you can read the latest `version` from.
+    packagesToBump: ['packages/*'],
+    packagesToPublish: ['packages/*'],
+  },
 };
