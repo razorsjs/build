379f03666bc7876acf2cd3027116305500f8db9b
liujia02 <bit_liujia@163.com>
2020-08-13 14:38:20 +0800

    ci(circle): add test workflow

--- packages/build-jest/index.js
+++ packages/build-jest/index.js
@@ -1,21 +1,8 @@
 export default function(options) {
-  let localConfig = {
-    moduleFileExtensions: [
-      "js",
-      "json"
-    ],
-    transform: {
-      "^.+\\.[t|j]sx?$": "babel-jest"
-    }
-  }
-  if(options.useVue){
-    localConfig.moduleFileExtensions.push('vue')
-    localConfig.transform = {
-      ...localConfig.transform,
-      ...{
-        ".*\\.(vue)$": "vue-jest"
-      }
-    }
+  let localConfig = {}
+  options = options || {}
+  if(options.typescript) {
+    localConfig.preset = 'ts-jest'
   }
   return localConfig
 }
